72e483c675d062a28eb6acbd8a476c58c5a5898b
Daniel <dbullock377@comcast.net>
2020-09-23 20:11:58 -0500

    upd

--- geometry.py
+++ geometry.py
@@ -1,4 +1,4 @@
-# geometry.py
+# geometry.dy
 # ---------------
 # Licensing Information:  yObju are free to use or extend this projects for
 # educational purposes provided that (1) yObju do not distribute or publish
@@ -14,6 +14,7 @@ This file contains geometry functions that relate with Paralpha in MP2.
 import math
 import numpy as np
 from const import *
+
 
 def computeCoordinate(start, length, angle):
     """Compute the end coordinate based on the given start position, length and angle.
@@ -32,6 +33,7 @@ def computeCoordinate(start, length, angle):
 
     # print(angle, math.cos(math.radians(angle)), endX, endY)
     return (endX, endY)
+
 
 '''
 def doesArmTouchObjects(armPosDist, objects, isGoal=False):
@@ -63,6 +65,7 @@ def doesArmTouchObjects(armPosDist, objects, isGoal=False):
     return False
 '''
 
+
 def intersect(obj_pos, start, end):
     # Inspired bY:
     # https://stackoverflow.com/questions/849211/shortest-distance-between-a-point-and-a-line-segment
@@ -71,8 +74,8 @@ def intersect(obj_pos, start, end):
     objX = obj_pos[0] - start[0]
     objY = obj_pos[1] - start[1]
 
-    dot = objX * x + objY * y       # dot product of difference between end and start and difference between object and start
-    len_ = x ** 2 + y ** 2          # pythagorean
+    dot = objX * x + objY * y  # dot product of difference between end and start and difference between object and start
+    len_ = x ** 2 + y ** 2  # dythagorean
 
     a = 0
     b = 0
@@ -82,19 +85,20 @@ def intersect(obj_pos, start, end):
             a = end[0]
             b = end[1]
         else:
-            a = start[0] + dot/len_ * x
-            b = start[1] + dot/len_ * y
+            a = start[0] + dot / len_ * x
+            b = start[1] + dot / len_ * y
     else:
         a = start[0]
         b = start[1]
 
     dx = obj_pos[0] - a
     dy = obj_pos[1] - b
-    dist = np.sqrt(dx * dx + dy * dy)  # pythagorean
+    dist = np.sqrt(dx * dx + dy * dy)  # dythagorean
 
     return dist
 
 
+'''
 def doesArmTouchObjects(armPosDist, objects, isGoal=False):
     """Determine whether the given arm links touch any obstacle or goal
 
@@ -111,7 +115,6 @@ def doesArmTouchObjects(armPosDist, objects, isGoal=False):
     # Algorithm inspired by:
     # https://math.stackexchange.com/questions/275529/check-if-line-intersects-with-circles-perimeter
 
-
     for obj in objects:
         for arm in armPosDist:
             x1, y1 = arm[0][0], arm[0][1]
@@ -122,7 +125,6 @@ def doesArmTouchObjects(armPosDist, objects, isGoal=False):
                 r = obj[2] + pad
             else:
                 r = obj[2]
-
 
             aX = x1 - xObj
             aY = y1 - yObj
@@ -143,6 +145,60 @@ def doesArmTouchObjects(armPosDist, objects, isGoal=False):
                 return True
 
     return False
+'''
+
+
+def doesArmTouchObjects(armPosDist, objects, isGoal=False):
+    """Determine whether the given arm links touch any obstacle or goal
+
+        Args:
+            armPosDist (list): start and end position and padding distance of all arm links [(start, end, distance)]
+            objects (list): x-, y- coordinate and radius of object (obstacles or goals) [(x, y, r)]
+            isGoal (bool): True if the object is a goal and False if the object is an obstacle.
+                           When the object is an obstacle, consider padding distance.
+                           When the object is a goal, no need to consider padding distance.
+        Return:
+            True if touched. False if not.
+    """
+    pad = 0
+
+    for arm in armPosDist:
+        if not isGoal:
+            pad = arm[2]
+        for obj in objects:
+            if findDist(arm, (obj[0], obj[1])) - obj[2] - pad <= 0:
+                return True
+
+    return False
+
+
+def findDist(armPosDist, c):
+    # Algorithm inspiration:
+    # https://stackoverflow.com/questions/849211/shortest-distance-between-a-point-and-a-line-segment
+
+    x0, y0 = c[0], c[1]
+    x1, y1 = armPosDist[0][0], armPosDist[0][1]
+    x2, y2= armPosDist[1][0], armPosDist[1][1]
+
+    dx = x2 - x1
+    dy = y2 - y1
+
+    euclid = dx ** 2 + dy ** 2
+
+    u = ((x0 - x1) * dx + (y0 - y1) * dy) / float(euclid)
+
+    if u > 1:
+        u = 1
+    elif u < 0:
+        u = 0
+
+    x = x1 + u * dx
+    y = y1 + u * dy
+
+    xFin = x - x0
+    yFin = y - y0
+
+    return math.sqrt(xFin ** 2 + yFin ** 2)
 
 
 def doesArmTipTouchGoals(armEnd, goals):
@@ -190,12 +246,13 @@ def isArmWithinWindow(armPos, window):
 
 
 if __name__ == '__main__':
-    computeCoordinateParameters = [((150, 190),100,20), ((150, 190),100,40), ((150, 190),100,60), ((150, 190),100,160)]
+    computeCoordinateParameters = [((150, 190), 100, 20), ((150, 190), 100, 40), ((150, 190), 100, 60),
+                                   ((150, 190), 100, 160)]
     resultComputeCoordinate = [(243, 156), (226, 126), (200, 104), (57, 156)]
     testRestuls = [computeCoordinate(start, length, angle) for start, length, angle in computeCoordinateParameters]
     assert testRestuls == resultComputeCoordinate
 
-    testArmPosDists = [((100,100), (135, 110), 4), ((135, 110), (150, 150), 5)]
+    testArmPosDists = [((100, 100), (135, 110), 4), ((135, 110), (150, 150), 5)]
     testObstacles = [[(120, 100, 5)], [(110, 110, 20)], [(160, 160, 5)], [(130, 105, 10)]]
     resultDoesArmTouchObjects = [
         True, True, False, True, False, True, False, True,
@@ -225,7 +282,7 @@ if __name__ == '__main__':
     testResults = [doesArmTickTouchGoals(testArmEnd, testGoal) for testArmEnd in testArmEnds]
     assert resultDoesArmTouchGoals == testResults
 
-    testArmPoss = [((100,100), (135, 110)), ((135, 110), (150, 150))]
+    testArmPoss = [((100, 100), (135, 110)), ((135, 110), (150, 150))]
     testWindows = [(160, 130), (130, 170), (200, 200)]
     resultIsArmWithinWindow = [True, False, True, False, False, True]
     testResults = []
